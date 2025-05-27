0229b0fed4d4aeddd846d0d2918a8123c2ab83d0
Ramy Phoenix <recoveryfreenethub@gmail.com>
2025-05-27 19:00:36 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,71 @@
+//base by DGXeon (Xeon Bot Inc.)
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: @DGXeon
+//Instagram: unicorn_xeon13
+//Telegram: t.me/xeonbotinc
+//GitHub: @DGXeon
+//WhatsApp: +919366316018
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+
+//contact details
+global.ownernomer = "25761787221" //owner number
+global.ownername = "Itachi Uchiha" //owner name
+global.dev = ["25761787221"]
+global.ownername = "Itachi Uchiha"
+global.ytname = "YT: Freenethub Tech" //youtube channel name
+global.socialm = "GitHub: Ramy Phoenix" //github username
+global.location = "India, Mizoram, Aizawl"
+
+global.ownernumber = '25761787221'  //creator number
+global.ownername = 'Itachi Uchiha' //owner name
+global.botname = 'WhatsApp Bug' //name of the bot
+
+//sticker details
+global.packname = '\n\n\n\n\n\n\nSticker By'
+global.author = 'Itachi Uchiha\n\nContact: +25761787221'
+
+//console view/theme
+global.themeemoji = '🪀'
+global.wm = "Itachi Uchiha Inc."
+
+//theme link
+global.link = 'https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20'
+
+//prefix
+global.prefa = ['','!','.',',','🐤','🗿'] 
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 20
+}
+
+//menu type 
+//v1 is image menu, 
+//v2 is link + image menu,
+//v3 is video menu,
+//v4 is call end menu
+global.typemenu = 'v1'
+
+// Global Respon
+global.mess = {
+    success: 'Done✓',
+    admin: `\`[ # ]\` This Command Can Only Be Used By Group Admins !`,
+    botAdmin: `\`[ # ]\` This Command Can Only Be Used When Bot Becomes Group Admin !`,
+    OnlyOwner: `\`[ # ]\` This Command Can Only Be Used By Premium User ! \n\nWant Premium? Chat Developer.\nYouTube: @DGXeon\nTelegram: @DGXeon13\nWhatsApp: +916909137213`,
+    OnlyGrup: `\`[ # ]\` This Command Can Only Be Used In Group Chat !`,
+    private: `\`[ # ]\` This Command Can Only Be Used In Private Chat !`,
+    wait: `\`[ # ]\` Wait Wait a minute`,
+    notregist: `\`[ # ]\` You are not registered in the Bot Database. Please register first.`,
+    premium: `\`[ # ]\` This Command Can Only Be Used By Premium User ! \n\nWant Premium? Chat Developer.\nYouTube: @DGXeon\nTelegram: @DGXeon13\nWhatsApp: +916909137213`,
+}
+
+global.banner = ["447893985392@s.whatsapp.net","995544996873@s.whatsapp.net","916909137213","919366316018@s.whatsapp.net","919485490229@s.whatsapp.net","919402104403@s.whatsapp.net"]
+
+let fs = require('fs')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update ${__filename}`)
+delete require.cache[file]
+require(file)
+})
---
+++ settings.js
@@ -0,0 +1,45 @@
+//base by DGXeon (Xeon Bot Inc.)
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: @DGXeon
+//Instagram: unicorn_xeon13
+//Telegram: @DGXeon
+//GitHub: @DGXeon
+//WhatsApp: +916909137213
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//telegram channel: https://t.me/+WEsVdEN2B9w4ZjA9
+
+const fs = require("fs");
+const chalk = require("chalk")
+
+global.BOT_TOKEN = "8016060285:AAFczUJOouPwaSpF7Il6fkAldBraLVqzIrU" // create bot here https://t.me/Botfather and get bot token
+global.BOT_NAME = "WhatsApp Bug" //your bot name
+global.OWNER_NAME = "@Am_itachiuchiha" //your name with sign @
+global.OWNER = ["https://t.me/Nextgenroom", "https://www.youtube.com/@Freenethubtech"] // Make sure the username is correct so that the special owner features can be used.
+global.DEVELOPER = ["5962658076"] //developer telegram id to operate addprem delprem and listprem
+global.pp = 'https://i.ibb.co/ydRKHnw/thumb.jpg' //your bot pp
+
+
+//approval
+global.GROUP_ID = -1002230970689; // Replace with your group ID
+global.CHANNEL_ID = -1002380953539; // Replace with your channel ID
+global.GROUP_LINK = "https://t.me/Nextgenroom"; // Replace with your group link
+global.CHANNEL_INVITE_LINK = "https://t.me/Megahubbots"; // Replace with your private channel invite link
+global.WHATSAPP_LINK = "https://chat.whatsapp.com/Iwau9IDlCn4CR6fsmI3mc7"; // Replace with your group link
+global.YOUTUBE_LINK = "https://youtube.com/@Freenethubtech"; // Replace with your youtube link
+global.INSTAGRAM_LINK = "https://www.instagram.com/ramy_phoenix/"; // Replace with your ig link
+
+global.owner = global.owner = ['25761787221'] //owner whatsapp
+
+const {
+   english
+} = require("./lib");
+global.language = english
+global.lang = language
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update ${__filename}`))
+delete require.cache[file]
+require(file)
+})
